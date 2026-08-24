2628b8b33763639de3a863a53204ba1d90664a6c
adettori <2501979+adettori@users.noreply.github.com>
2026-08-24 10:50:23 +0200

    feat(depth-estimation-video): use personal fork of VDA in docker

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,13 +1,10 @@
 #! /bin/bash
 # Setup dirs
-mkdir -p ${DATASET}/rgbd-video
 mkdir -p ${DATASET}/rgb
 mkdir -p ${DATASET}/depth
+mkdir -p ${DATASET}/masks
 
 # Run depth estimation model
 docker run --rm --runtime=nvidia -v ${DATASET}:/app/input --user ${uid}:${gid} depth-estimation-video:latest
-# Extract frames from depth video and original video
-ffmpeg -i ${DATASET}/rgbd-video/video_src.mp4 -vf fps=20 ${DATASET}/rgb/frame_%04d.png
-ffmpeg -i ${DATASET}/rgbd-video/video_vis.mp4 -vf format=gray,fps=20 ${DATASET}/depth/frame_%04d.png
 # Run mask generation model
 docker run --rm --runtime=nvidia -e MASK_PROMPT="${OBJECT_PROMPT}" -v ${DATASET}:/app/input --user ${uid}:${gid} mask-generation:latest
