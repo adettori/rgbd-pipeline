4dfeae9ec4dc001f56991325ff00bedf0a687a5c
adettori <2501979+adettori@users.noreply.github.com>
2026-09-14 00:00:53 +0200

    chore: update pipeline scripts

--- build-pipeline.sh
+++ build-pipeline.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 docker build -t mask-generation mask-generation/
-docker build -t depth-completion depth-completion/
+docker build -t postprocess-capture postprocess-capture/
--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+BAG_DIR=./
+BAG_NAME="bag.db3"
+DATASET_DIR=bag_output
 MASK_PROMPT="black box near hands"
-DATASET_DIR=./ref_views/black-aligned
 
+mkdir -p $DATASET_DIR
+docker run --rm --runtime=nvidia -v $BAG_DIR:/app/input -e BAG_NAME="$BAG_NAME" -e DATASET_DIR="$DATASET_DIR" --user ${uid}:${gid} postprocess-capture:latest bash -c "python /app/postprocess.py /app/input/$BAG_NAME /app/input/$DATASET_DIR"
 docker run --rm --runtime=nvidia -v $DATASET_DIR:/app/input -e MASK_PROMPT="$MASK_PROMPT" -v ./rgbd-pipeline/mask-generation/XMem:/app/XMem --user ${uid}:${gid} mask-generation:latest
-
-docker run --rm --runtime nvidia -v $DATASET_DIR:/app/input -v ./rgbd-pipeline/depth-completion/DepthLab:/app/DepthLab --user ${uid}:${gid} depth-completion:latest
