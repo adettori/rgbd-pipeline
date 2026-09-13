13df7621d38ab00b0993c173a7eddaaa188055ce
adettori <2501979+adettori@users.noreply.github.com>
2026-09-13 13:48:22 +0200

    chore: tweak scripts

--- build-pipeline.sh
+++ build-pipeline.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker build -t depth-estimation-video depth-estimation-video/  
 docker build -t mask-generation mask-generation/
+docker build -t depth-completion depth-completion/
--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
-# Run depth estimation model
-docker run --rm --runtime=nvidia -v ${DATASET}:/app/input --user ${uid}:${gid} depth-estimation-video:latest
-# Run mask generation model
-docker run --rm --runtime=nvidia -e MASK_PROMPT="${MASK_PROMPT}" -v ./rgbd-pipeline/mask-generation/XMem:/app/XMem -v ${DATASET_DIR}:/app/input --user ${uid}:${gid} mask-generation:latest
+
+MASK_PROMPT="black box near hands"
+DATASET_DIR=./ref_views/black-aligned
+
+docker run --rm --runtime=nvidia -v $DATASET_DIR:/app/input -e MASK_PROMPT="$MASK_PROMPT" -v ./rgbd-pipeline/mask-generation/XMem:/app/XMem --user ${uid}:${gid} mask-generation:latest
+
+docker run --rm --runtime nvidia -v $DATASET_DIR:/app/input -v ./rgbd-pipeline/depth-completion/DepthLab:/app/DepthLab --user ${uid}:${gid} depth-completion:latest
